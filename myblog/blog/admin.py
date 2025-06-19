dbf152660db25bfe842ca8deccf97c661d09e2ee
pujakadayatdata <pujakadayat1@gmail.com>
2025-06-19 21:45:56 +0545

    Ui milauna baki

---
+++ myblog/blog/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from .models import *
+
+
+# Register your models here.
+
+
+# FOR CONFIGURATION OF CATEGORY ADMIN
+# admin option enable garna yesko class ko yesko parent banako
+class CategoryAdmin(admin.ModelAdmin):
+    # kun kun property admin table  jasari dekhauna tyo list ma lekhne
+    list_display = ('image_tag','title','description', 'urls','add_date')
+    search_fields = ('title',)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+    list_filter = ('cat',)
+    list_per_page = 50
+
+
+    class Media:
+        js = ('https://cdn.tiny.cloud/1/no-api-key/tinymce/7/tinymce.min.j','js/script.js',)
+
+# CUSTOMADMIN CATEGORY PASS GARE Category TA HAMRO MAIN MODEL VAYO TARA HAMILE SUTOMADMIN MODEL BANAKO XAU TESLAI BADI PREFERENCE MILXA
+admin.site.register(Category,CategoryAdmin)
+
+admin.site.register(Post, PostAdmin)
---
+++ myblog/blog/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Post
+
+
+
+# Create your views here.
+def home(request):
+    # load all the post from db(10)
+    posts= Post.objects.all()[:11]
+    # print(posts)
+    data = {
+        'posts':posts
+    }
+    return render(request, 'home.html',data)
+
+def post(request,url):
+    post = Post.objects.get(url=url)
+    return render(request, 'posts.html', {'post':post})
