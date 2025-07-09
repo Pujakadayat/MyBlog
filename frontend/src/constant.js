4302165f846398576af1433bbf826003cffc3fa5
pujakadayatdata <pujakadayat1@gmail.com>
2025-07-09 08:57:29 +0545

    sakiyo

--- frontend/src/constant.js
+++
@@ -1,6 +0,0 @@
-// the reason we are putting this here is were going to use the local storage to store access token and refresh token in our browser and i want to have key
-// that i can use to access in my local storage and that's what this key is going to be.I'm going to use this key to access the access token and refresh token
-
-
-export const ACCESS_TOKEN = "access";
-export const REFRESH_TOKEN = "refresh";
